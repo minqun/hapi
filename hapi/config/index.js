f7010fee474c8f0dd1bfce0d1cf1516ebc45de74
chenminqun <chenminqun@batmobi.net>
2018-12-29 18:06:38 +0800

    build

--- hapi/config/index.js
+++ hapi/config/index.js
@@ -8,5 +8,7 @@ const config = {
   host: env.HOST,
   port: env.PORT,
   jwtSecret: env.JWT_SECRET,
+  wxSecret: env.WX_APPSECRET,
+  wxAppid: env.APP_ID,
 };
 module.exports = config;
--- hapi/migrations/20181228081033-create-users-table.js
+++ hapi/migrations/20181228081033-create-users-table.js
@@ -1,38 +1,39 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-      return  queryInterface.createTable('users', {
-          id: {
-              type: Sequelize.INTEGER,
-              autoIncrement: true,
-              primaryKey: true,
-          },
-          nick_name: Sequelize.STRING,
-          avatar_url: Sequelize.STRING,
-          gender:Sequelize.INTEGER,
-          open_id:Sequelize.STRING,
-          session_key:Sequelize.STRING,
-          created_at: Sequelize.DATE,
-          updated_at: Sequelize.DATE,
-      })
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('users', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                comment: '用户ID',
+            },
+            nick_name: { type: Sequelize.STRING, comment: '用户昵称', },
+            avatar_url: { type: Sequelize.STRING, comment: '头像', },
+            gender: { type: Sequelize.INTEGER, comment: '性别', },
+            open_id: { type: Sequelize.STRING, comment: '用户openId', },
+            session_key: { type: Sequelize.STRING, comment: '用户session_key', },
+            created_at: { type: Sequelize.DATE, comment: '创建时间', },
+            updated_at: { type: Sequelize.DATE, comment: '更新时间', },
+        })
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
+          Example:
+          return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+        */
+    },
 
-  down: (queryInterface, Sequelize) => {
-    queryInterface => queryInterface.dropTable('users'),
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('users')
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+          Example:
+          return queryInterface.dropTable('users');
+        */
+    }
 };
